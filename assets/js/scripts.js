2b01c571502dd444419c7453928f54b146354978
Erick Renaud <renaud_junior@outlook.com>
2024-05-21 08:15:00 -0400

    Added displaySearchHistory function

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,14 +2,23 @@ var searchHistoryList = JSON.parse(localStorage.getItem('searchHistory'));
 const searchHistoryEl = $('#searchHistory');
 
 function saveSearchHistory() {
-    if(!searchHistoryList){
-        searchHistoryList = [];
-    }
-
     const newCity = $('#citySearch').val();
 
     searchHistoryList.push(newCity);
     localStorage.setItem('searchHistory', JSON.stringify(searchHistoryList));
 }
 
+function displaySearchHistory() {
+    if(!searchHistoryList){
+        searchHistoryList = [];
+    }
+    
+    for (var search of searchHistoryList) {
+        const searchCard = $('<div>').addClass('border m-2 bg-secondary text-center rounded-1');
+        searchCard.text(search);
+        searchHistoryEl.append(searchCard);
+    }
+}
+
+displaySearchHistory();
 $('.btn-search').click(saveSearchHistory);
