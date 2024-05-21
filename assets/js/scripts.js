ce32f29a3eb63c7ca831506e2582e62399e69d3c
Erick Renaud <renaud_junior@outlook.com>
2024-05-21 08:55:03 -0400

    added display and generate function with temporary cards

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -20,5 +20,54 @@ function displaySearchHistory() {
     }
 }
 
-displaySearchHistory();
-$('.btn-search').click(saveSearchHistory);
+function displayForecastCard() {
+    // everything is temporary for now until i start fetching from the api
+    const mainCard = $('#mainCard');
+    const mainCardCity = $('#cityName');
+    const mainCardTemp = $('#temp');
+    const mainCardWind = $('#wind');
+    const mainCardHumidity = $('#humidity');
+
+    const city = 'alexandria';
+    const temp = '72';
+    const wind = 'lots';
+    const humidity = '73';
+
+    mainCardCity.text(city);
+    mainCardTemp.text(temp);
+    mainCardWind.text(wind);
+    mainCardHumidity.text(humidity);
+
+    const forecastCardArea = $('#forecastCards');
+
+    for (let i = 0; i < 5; i++) {
+        forecastCardArea.append(generateCard())
+    }
+}
+
+function generateCard() {
+    // everything is temporary for now until i start fetching from the api
+    const cardDate = $('<p>').addClass('fw-bold m-0');
+    const cardEmoji = $('<p>').addClass('m-0');
+    const cardTemp = $('<p>').addClass('m-0');
+    const cardWind = $('<p>').addClass('m-0');
+    const cardHumidity = $('<p>').addClass('m-0 mb-1');
+    const cardSection = $('<section>').addClass('card col-2 bg-secondary rounded-0 text-white');
+
+    cardDate.text('05/06/2024');
+    cardEmoji.text('sun');
+    cardTemp.text('56');
+    cardWind.text('45');
+    cardHumidity.text('78');
+
+    cardSection.append(cardDate, cardEmoji, cardTemp, cardWind, cardHumidity);
+
+    return cardSection;
+}
+
+$(document).ready(function (){
+    displayForecastCard();
+    displaySearchHistory();
+    $('.btn-search').click(saveSearchHistory);
+});
+
